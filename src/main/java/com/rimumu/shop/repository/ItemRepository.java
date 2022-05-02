333010059a7c18a53573d29517e8a758d3cf4c8b
CodeRothy <yiwooy@naver.com>
2022-05-02 19:26:24 +0900

    ~ JPQL 1

---
+++ src/main/java/com/rimumu/shop/repository/ItemRepository.java
@@ -0,0 +1,30 @@
+package com.rimumu.shop.repository;
+
+import com.rimumu.shop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+
+    List<Item> findByItemNm(String itemNm);
+
+    // Or
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    // LessThan
+    List<Item> findByPriceLessThan(Integer price);
+
+    // OrderBy
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    // @Query JPQL
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    @Query(value = "select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+}

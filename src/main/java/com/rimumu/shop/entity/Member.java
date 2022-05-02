f36a6dbc2b4d353516f78e48a176b1b6bf89f5fc
CodeRothy <yiwooy@naver.com>
2022-05-03 00:56:11 +0900

    회원가입 @Valid

---
+++ src/main/java/com/rimumu/shop/entity/Member.java
@@ -0,0 +1,47 @@
+package com.rimumu.shop.entity;
+
+import com.rimumu.shop.constant.Role;
+import com.rimumu.shop.dto.MemberFormDto;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter
+@Setter
+@ToString
+public class Member {
+
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    //@Builder 로 변경 예쩡
+    public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder) {
+        Member member = new Member();
+        member.setName(memberFormDto.getName());
+        member.setEmail(memberFormDto.getEmail());
+        member.setAddress(memberFormDto.getAddress());
+        String password = passwordEncoder.encode(memberFormDto.getPassword());
+        member.setPassword(password);
+        member.setRole(Role.USER);
+        return member;
+    }
+}
---
+++ src/main/java/com/rimumu/shop/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.rimumu.shop.repository;
+
+import com.rimumu.shop.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByEmail(String email);
+
+}

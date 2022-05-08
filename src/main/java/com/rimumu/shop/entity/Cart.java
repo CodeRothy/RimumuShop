bb800b3f8020bec806808e7a12fd432c94a95308
CodeRothy <yiwooy@naver.com>
2022-05-08 23:44:23 +0900

    cart 1

---
+++ src/main/java/com/rimumu/shop/entity/Cart.java
@@ -0,0 +1,26 @@
+package com.rimumu.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter @Setter
+@ToString
+public class Cart {
+
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+
+
+}

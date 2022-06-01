d5b571faaa8d2394af1c543376671a97bac500fe
CodeRothy <yiwooy@naver.com>
2022-06-01 23:03:32 +0900

    cart 장바구니 조회 해결(쿼리문 공백, gradle)

--- src/main/java/com/rimumu/shop/repository/CartItemRepository.java
+++ src/main/java/com/rimumu/shop/repository/CartItemRepository.java
@@ -12,11 +12,11 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findByCartIdAndItemId(Long cartId, Long itemId);
 
     @Query("select new com.rimumu.shop.dto.CartDetailDto(ci.id, i.itemNm,i.price, ci.count, im.imgUrl)" +
-            "from CartItem ci, ItemImg im" +
-            "join ci.item i" +
-            "where ci.cart.id = :cartId" +
-            "and im.item.id = ci.item.id" +
-            "and im.repImgYn = 'Y'" +
+            "from CartItem ci, ItemImg im " +
+            "join ci.item i " +
+            "where ci.cart.id = :cartId " +
+            "and im.item.id = ci.item.id " +
+            "and im.repImgYn = 'Y' " +
             "order by ci.regTime desc")
     List<CartDetailDto> findCartDetailDtoList(Long cartId);
 
